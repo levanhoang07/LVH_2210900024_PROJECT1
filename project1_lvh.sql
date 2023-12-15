b7b69cfd7fdd43147017651a62995b417fa48239
levanhoang07 <levanhoang742004@gmail.com>
2023-12-15 21:29:27 +0700

    levanhoang

--- project1_lvh.sql
+++ project1_lvh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 15, 2023 lúc 02:57 PM
+-- Thời gian đã tạo: Th12 15, 2023 lúc 03:27 PM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -20,6 +20,39 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `project1_lvh`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang_lvh`
+--
+
+CREATE TABLE `khachhang_lvh` (
+  `MAKH_LVH` int(11) NOT NULL,
+  `HOTEN_LVH` varchar(100) NOT NULL,
+  `TAIKHOAN_LVH` varchar(50) NOT NULL,
+  `MATKHAU_LVH` varchar(30) NOT NULL,
+  `DIACHI_LVH` varchar(200) NOT NULL,
+  `DIENTHOAI_LVH` varchar(30) NOT NULL,
+  `EMAIL_LVH` varchar(50) NOT NULL,
+  `NGAYSINH_LVH` datetime NOT NULL,
+  `NGAYCAPNHAT_LVH` datetime NOT NULL,
+  `GIOITINH_LVH` tinyint(4) NOT NULL,
+  `TICHDIEM_LVH` int(11) NOT NULL,
+  `TRANGTHAI_LVH` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `quantri_lvh`
+--
+
+CREATE TABLE `quantri_lvh` (
+  `TAIKHOAN_LVH` varchar(50) NOT NULL,
+  `MATKHAU_LVH` varchar(50) NOT NULL,
+  `TRANGTHAI_LVH` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -48,6 +81,18 @@ INSERT INTO `sanpham_lvh` (`MA_LVH`, `TEN_LVH`, `SOLUONG_LVH`, `DONGIA_LVH`, `TR
 --
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `khachhang_lvh`
+--
+ALTER TABLE `khachhang_lvh`
+  ADD PRIMARY KEY (`MAKH_LVH`);
+
+--
+-- Chỉ mục cho bảng `quantri_lvh`
+--
+ALTER TABLE `quantri_lvh`
+  ADD PRIMARY KEY (`TAIKHOAN_LVH`);
 
 --
 -- Chỉ mục cho bảng `sanpham_lvh`
